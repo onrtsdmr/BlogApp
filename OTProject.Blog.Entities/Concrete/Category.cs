1f165880016318f23077f3a0a3b7dab9a0f959d9
onrtsdmr <onurtaademir@gmail.com>
2020-07-28 16:44:20 +0300

    Added category model file.

--- OTProject.Blog.Entities/Concrete/Category.cs
+++ OTProject.Blog.Entities/Concrete/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace OTProject.Blog.Entities.Concrete
+﻿using OTProject.Blog.Entities.Interfaces;
+
+namespace OTProject.Blog.Entities.Concrete
 {
-    public class Category
+    public class Category: ITable
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
